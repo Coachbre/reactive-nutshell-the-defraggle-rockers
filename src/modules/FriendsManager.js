5f8e6b83944a8d4b50daea713abf3e12e2470bb3
MegMcGregor <megmcgregor1@gmail.com>
2021-04-13 15:52:00 -0500

    began API calls

--- src/modules/FriendsManager.js
+++ src/modules/FriendsManager.js
@@ -1,1 +1,11 @@
 const remoteURL = "http://localhost:8088"
+
+export const getAllFriends = () => {
+    return fethch (`${remoteURL}/friendships`)
+    .then(res => res.json());
+}
+
+export const getFriendById = (friendshipId) => {
+    return fethch (`${remoteURL}/friendships/${userId}`)
+    .then(res => res.json());
+}
