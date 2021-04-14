87a954fcafed6bb208dbfc7127b60955bf1fc470
MegMcGregor <megmcgregor1@gmail.com>
2021-04-14 14:23:45 -0500

    friends list automation

--- src/components/friend/FriendCard.js
+++ src/components/friend/FriendCard.js
@@ -3,10 +3,11 @@ import "./Friend.css";
 import { Link } from "react-router-dom";
 
 export const FriendCard = ({friend}) => {
+    console.log(friend)
     return (
-        <section className="friend">
+        <section className="friend-card">
             <img alt="friend_image"/>
-            <h4>{friend.id}</h4>
+            <h4>{friend.user.name}</h4>
             <button>Delete</button>
         </section>
     )
