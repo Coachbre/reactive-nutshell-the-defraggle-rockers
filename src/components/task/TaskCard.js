4b2a5e47514cf77234f30066901a2887961baa49
Coachbre <brekonan1@gmail.com>
2021-04-19 16:50:55 -0400

    added author comments and changed task css

--- src/components/task/TaskCard.js
+++ src/components/task/TaskCard.js
@@ -1,3 +1,6 @@
+//author: Bre Coach
+// shows individual task info along with delete and edit options
+
 import React from 'react'
 import { Link } from 'react-router-dom'
 import "./Task.css"
@@ -5,26 +8,33 @@ import "./Task.css"
 
 
 
-export const TaskCard = ({task, handleDelete, handleHideTask, handleEditTask}) => (
-    //'task, handleDelete, handleHideTask, handleEditTask' are props being passed in from TaskList() return
+export const TaskCard = ({task, handleDelete, handleHideTask}) => (
+    //'task, handleDelete, handleHideTask' are props being passed in from TaskList() return
 
     <section className="task">
-        <h2 className="task_name">{task.name}</h2>
+      
+        <li><h2 className="task_name">{task.name}</h2>
  
         <h3 className="task_dueDate">Complete by: {task.dueDate}</h3>
 
         <div>   
             <label>Complete</label>
             <input type="checkbox" id="checkbox" onChange={() => handleHideTask(task)}/>
-            <br></br>
+            
         </div>
 
+        <div>
         <Link to={`/tasks/edit/${task.id}`}>
             {/* sets the URL to match the specific task id # */}
-        <button type="button">Edit Task</button>
+        <button type="button" className="btn-primary">Edit Task</button>
         </Link>
+        </div>
 
-        <button type="button" onClick={() => handleDelete(task.id)}>Delete Task</button>
+        <div>
+        <button type="button" className="btn-primary" onClick={() => handleDelete(task.id)}>Delete Task</button>
+        </div>
+        </li>
+        
     </section> //why is onClick formatted like this? ^
 )
 
